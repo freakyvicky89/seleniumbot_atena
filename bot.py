f553d46f12a6cdfb47ba449780ccd94c0f477bbf
freakyvicky <freakyvicky89@gmail.com>
2020-05-01 20:34:25 +0200

    goes to random frontpage news

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import sys
+from random import randrange
 from time import sleep
-
 from selenium import webdriver
 
 if len(sys.argv) > 1 and sys.argv[1]:
@@ -15,7 +15,12 @@ if not development:
 
 driver = webdriver.Chrome(options=driver_options)
 driver.get('https://www.wp.pl/')
+sleep(2)
+driver.find_element_by_xpath('//button[contains(.,"AKCEPTUJ")]').click()
+sleep(2)
+news_links = driver.find_elements_by_xpath('//div[@data-st-area="Wiadomosci"]//a')
+news_links[randrange(0, len(news_links))].click()
 
-sleep(10)
 
-driver.close()
+
+#driver.close()
